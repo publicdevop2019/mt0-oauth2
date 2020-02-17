cebcf6a78b98c19efaa69c6fd6af5a5e9ff62a59
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-02-16 19:18:12 -0500

    update shared content

--- src/main/java/com/hw/shared/OAuthRestTemplateConfig.java
+++ src/main/java/com/hw/shared/OAuthRestTemplateConfig.java
@@ -1,12 +1,9 @@
-package com.hw.config;
+package com.hw.shared;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
