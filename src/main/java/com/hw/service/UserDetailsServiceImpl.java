699a1dedc57f0cb87479d483ef51c13458c42ce3
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-02-29 18:23:09 -0500

    add trace

--- src/main/java/com/hw/service/UserDetailsServiceImpl.java
+++ src/main/java/com/hw/service/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hw.service;
 
+import com.hw.entity.ResourceOwner;
 import com.hw.repo.ResourceOwnerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +15,17 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     ResourceOwnerRepo resourceOwnerRepo;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return resourceOwnerRepo.findOneByEmail(username);
+    public UserDetails loadUserByUsername(String usernameOrId) throws UsernameNotFoundException {
+        try {
+            return resourceOwnerRepo.findById(Long.parseLong(usernameOrId)).get();
+        } catch (NumberFormatException ex) {
+            /**
+             * is email
+             */
+            ResourceOwner oneByEmail = resourceOwnerRepo.findOneByEmail(usernameOrId);
+            if (oneByEmail == null)
+                throw new UsernameNotFoundException("unable to find user :: " + usernameOrId);
+            return oneByEmail;
+        }
     }
 }
