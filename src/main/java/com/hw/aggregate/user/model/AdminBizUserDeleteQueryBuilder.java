9f7d9c69867730dbfffeb27c22daa623eb97b29a
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-10-13 10:54:55 -0400

    update shared

--- src/main/java/com/hw/aggregate/user/model/AdminBizUserDeleteQueryBuilder.java
+++ src/main/java/com/hw/aggregate/user/model/AdminBizUserDeleteQueryBuilder.java
@@ -1,13 +1,13 @@
 package com.hw.aggregate.user.model;
 
-import com.hw.shared.sql.builder.DeleteByIdQueryBuilder;
+import com.hw.shared.sql.builder.SoftDeleteQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 
 @Component
-public class AdminBizUserDeleteQueryBuilder extends DeleteByIdQueryBuilder {
+public class AdminBizUserDeleteQueryBuilder extends SoftDeleteQueryBuilder<BizUser> {
     @Autowired
     private void setEntityManager(EntityManager entityManager) {
         em = entityManager;
